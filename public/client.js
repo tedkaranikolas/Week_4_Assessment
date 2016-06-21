ff1fae0862082acc377afc654d97f21ffe9467cd
Ted Karanikolas <ted@hrg-nyc.com>
2016-06-20 21:04:18 -0500

    first commit, DB returns null animal

---
+++ public/client.js
@@ -0,0 +1,37 @@
+console.log('hello from client.js');
+
+$(document).ready (function(){
+
+  console.log('hello from jquery');
+
+$('#submitAnimal').on('click', animalOut);
+getAnimal();
+
+function animalOut(){
+    var animal = $('#animalIn').val();
+    var objectOut = {
+      "animal" : animal,
+    };
+    $.ajax({
+      type: 'POST',
+      url: '/postAnimals'
+    });
+}
+
+function getAnimal(){
+    $.ajax({
+      type: 'GET',
+      url: '/getAnimal',
+      success: function(data){
+        animalDisplay(data);
+    }
+  });
+}//end function get animal
+
+function animalDisplay(animal){
+  $('#animalIn').val();
+  for( var i=0; i<animal.length; i++){
+    $('#outputDiv').append('<p>Animal: ' + animal[i].animal + " , Amount: " + animal[i].animalcount + '</p>');
+  }
+}//end animalDisplay
+});//END JQUERY
---
+++ server/app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/animalchallenge';
+var number = require('../modules/numbergen.js');
+
+app.use(express.static('public'));
+
+//establish server
+app.listen( '3000', 'localhost', function(req, res){
+  console.log('listening on 3000');
+});
+
+//base URL
+app.get('/', function(req, res){
+  res.sendFile(path.resolve('views/index.html'));
+});
+
+//inserts animal entered from dom, via client.js into DB 'animals'
+app.post('/postAnimals', urlencodedParser, function(req, res){
+  console.log('Animal is sent to DB');
+  pg.connect(connectionString, function(err, client, done){
+    client.query( 'INSERT INTO animals (animal, animalcount) VALUES ($1, $2)', [req.body.animal, number(1, 100)]);
+    done();
+  });
+  res.end();
+});
+
+//return route for DOM
+app.get('/getAnimal', function(req, res){
+  console.log('sending animal back');
+  var results = [];
+  pg.connect(connectionString, function(err, client, done){
+  var animalRequest=client.query('SELECT * FROM animals ORDER BY id DESC;');
+  animalRequest.on('row', function(row){
+    results.push(row);
+  });//end grabbing DB row
+  animalRequest.on('end', function(){
+    return res.json(results);
+  });
+    if(err){
+      console.log(err);
+    }
+  });//end pg.connect
+});//end retrieveChore route
